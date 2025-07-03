0c99e1a55a45b3f61e4f0fb540ca6ba0563d5998
Anthony Sim <anthonyysim@gmail.com>
2025-07-02 22:54:17 -0700

    added redirect logic

---
+++ frontend/src/auth/auth.ts
@@ -0,0 +1,9 @@
+export const auth = {
+  isAuthenticated: false,
+  login() {
+    this.isAuthenticated = true;
+  },
+  logout() {
+    this.isAuthenticated = false;
+  },
+};
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,9 +3,13 @@ import {
   createRootRoute,
   createRoute,
   Outlet,
+  redirect,
 } from "@tanstack/react-router";
+
 import Todos from "./pages/Todos";
 import App from "./App";
+
+import { auth } from "./auth/auth";
 
 const rootRoute = createRootRoute({
   component: () => (
@@ -25,6 +29,12 @@ const todosRoute = createRoute({
   path: "/todos",
   getParentRoute: () => rootRoute,
   component: Todos,
+  loader: async () => {
+    if (!auth.isAuthenticated) {
+      throw redirect({ to: "/" });
+    }
+    return null;
+  },
 });
 
 const routeTree = rootRoute.addChildren([homeRoute, todosRoute]);
