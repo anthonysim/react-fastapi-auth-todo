4a49d2e20c3ee3d8ee4b7c165021b505038855ea
Anthony Sim <anthonyysim@gmail.com>
2025-07-03 16:23:33 -0700

    get all tasks and create task per user

--- backend/src/routes/todo_routes.py
+++ backend/src/routes/todo_routes.py
@@ -22,19 +22,27 @@ def add_task(
     db: Session = Depends(get_session),
     current_user: User = Depends(get_current_user)
 ):
+    """
+    Add a new task to the database.
+    - **todo**: The task details in the request body.
+    - **current_user**: The current login user.
+    - **Returns**: The created Todo item.
+    """
     return create_task(todo, db, current_user)
 
 
 @router.get("/tasks", response_model=list[Todo])
-def read_tasks(db: Session = Depends(get_session)):
-    return read_all_tasks(db)
+def read_tasks(
+    db: Session = Depends(get_session),
+    current_user: User = Depends(get_current_user)
+):
+    """
+    Read all user's tasks.
+    - **current_user**: The current login user.
+    - **Returns**: The user's tasks per user's id.
+    """
+    return read_all_tasks(db, user_id=current_user.id)
 
-# @app.get("/tasks", response_model=list[Todo])
-# def read_tasks(
-#     db: Session = Depends(get_session),
-#     current_user: User = Depends(get_current_user)
-# ):
-#     return read_all_tasks(db, user_id=current_user.id)
 
 @router.get("/task/{task_id}", response_model=Todo)
 def read_task(task_id: str, db: Session = Depends(get_session)):
