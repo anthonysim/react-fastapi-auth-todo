d2136345f0290a80ec56119e02e9b76da68b2485
Anthony Sim <anthonyysim@gmail.com>
2025-07-04 22:00:40 -0700

    remove newline

--- backend/src/routes/auth_routes.py
+++ backend/src/routes/auth_routes.py
@@ -51,7 +51,6 @@ async def register(user: UserCreate, db: AsyncSession = Depends(get_session)):
     }
 
 
-
 @router.post("/login")
 async def login(
     form_data: OAuth2PasswordRequestForm = Depends(),
